74a611757173d2b76d9e4d58356ef0bc6cf5cda9
Olya <ola.shy@yahoo.com>
2022-06-07 15:41:25 +0200

    fix

--- src/app/components/subjects/subjects-routing.module.ts
+++ src/app/components/subjects/subjects-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubjectsComponent } from './subjects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'subjects',
+    component: SubjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
