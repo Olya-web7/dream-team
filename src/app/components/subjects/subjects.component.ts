af999ef49d814e971aabfd0e184e8daf138dc45c
Olya <ola.shy@yahoo.com>
2022-06-07 21:45:30 +0200

    fix

--- src/app/components/subjects/subjects.component.ts
+++ src/app/components/subjects/subjects.component.ts
@@ -2,7 +2,8 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { SubjectsService } from 'src/app/services/subjects.service';
 import { SubjectModel } from './subject.model';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
+import {MatSort, Sort} from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 // import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
@@ -17,7 +18,7 @@ export class SubjectsComponent implements AfterViewInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private subjectsService: SubjectsService) {
+  constructor(private subjectsService: SubjectsService, private _liveAnnouncer: LiveAnnouncer) {
     this.dataSource = new MatTableDataSource(this.subjects);
   }
 
@@ -30,5 +31,18 @@ export class SubjectsComponent implements AfterViewInit {
       });
     // this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+   /** Announce the change in sort state for assistive technology. */
+   announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
