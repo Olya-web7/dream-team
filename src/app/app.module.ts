94db0cbe8a33312dd44a211d4a1751239a8ed93a
Daria Rusinska <drusi@softserveinc.com>
2022-06-07 16:34:51 +0200

    specialty initial commit, almost finished main component, started comfirmation-delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,20 @@ import { SpecialtiesComponent } from './components/specialties/specialties.compo
 import { MaterialModule } from './modules/material/material.module';
 import { NewSpecialtieComponent } from './components/specialties/new-specialtie/new-specialtie.component';
 import { SpecialtiesService } from './services/specialties.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfirmDeleteComponent } from './components/specialties/confirm-delete/confirm-delete.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SpecialtiesComponent,
-    NewSpecialtieComponent
+    NewSpecialtieComponent,
+    ConfirmDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, BrowserAnimationsModule, MaterialModule
+    AppRoutingModule, BrowserAnimationsModule, MaterialModule, HttpClientModule
   ],
   providers: [SpecialtiesService],
   bootstrap: [AppComponent]
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
   declarations: [],
-  imports: [], 
-  exports: []
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTableModule, MatPaginatorModule, MatDialogModule], 
+  exports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTableModule, MatPaginatorModule, MatDialogModule]
 })
 export class MaterialModule { }
