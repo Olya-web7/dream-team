87a6be502e512888348df1a1e9be33baeb58d8bd
Olya <ola.shy@yahoo.com>
2022-06-07 22:14:15 +0200

    fix

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,16 +5,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
-=======
-import {NgModule} from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatListModule} from "@angular/material/list";
->>>>>>> 853b77b52ac427a2ddd03e8d2aaf1523b47d0323
+
+// import {MatPaginatorModule} from '@angular/material/paginator';
+// import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
@@ -25,12 +17,7 @@ import {MatListModule} from "@angular/material/list";
     MatButtonModule,
     MatListModule,
     MatTableModule,
-<<<<<<< HEAD
     MatFormFieldModule,
-=======
-    MatPaginatorModule,
-    MatDialogModule,
->>>>>>> 853b77b52ac427a2ddd03e8d2aaf1523b47d0323
   ],
   exports: [
     MatToolbarModule,
@@ -38,16 +25,8 @@ import {MatListModule} from "@angular/material/list";
     MatButtonModule,
     MatListModule,
     MatTableModule,
-<<<<<<< HEAD
     MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
-=======
-    MatPaginatorModule,
-    MatDialogModule,
-  ],
-})
-export class MaterialModule {
-}
->>>>>>> 853b77b52ac427a2ddd03e8d2aaf1523b47d0323
+
