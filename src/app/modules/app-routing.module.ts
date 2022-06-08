1ca126d12f3f2bb69ab7d7dcfaf8ea04da95e0e2
Daria Rusinska <drusi@softserveinc.com>
2022-06-08 09:55:11 +0200

    Main specialties and confirm-delete done

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpecialtiesComponent } from '../components/specialties/specialties.component';
 import { SubjectsComponent } from '../components/subjects/subjects.component';
 import { HomePageComponent } from '../pages/home-page/home-page.component';
 
@@ -7,6 +8,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: HomePageComponent },
   { path: 'subjects', component: SubjectsComponent },
+  { path: 'speciality', component: SpecialtiesComponent },
 ];
 
 @NgModule({
