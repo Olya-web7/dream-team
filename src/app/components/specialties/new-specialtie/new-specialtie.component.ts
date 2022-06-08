0bbb733e11498afdd171bf721df29ca789e4e336
Olya <ola.shy@yahoo.com>
2022-06-08 12:18:22 +0200

    work on linter errors

--- src/app/components/specialties/new-specialtie/new-specialtie.component.ts
+++ src/app/components/specialties/new-specialtie/new-specialtie.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-specialtie',
   templateUrl: './new-specialtie.component.html',
   styleUrls: ['./new-specialtie.component.scss']
 })
-export class NewSpecialtieComponent implements OnInit {
+export class NewSpecialtieComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
