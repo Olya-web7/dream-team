d8b3f75f94f10d54e7e4ecbaf48df6aa94563cfc
Olya <ola.shy@yahoo.com>
2022-06-10 09:06:09 +0200

    husky

--- src/app/components/subjects/confirm-subject-delete/confirm-subject-delete.component.spec.ts
+++ src/app/components/subjects/confirm-subject-delete/confirm-subject-delete.component.spec.ts
@@ -3,22 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConfirmSubjectDeleteComponent } from './confirm-subject-delete.component';
 
 describe('ConfirmSubjectDeleteComponent', () => {
-    let component: ConfirmSubjectDeleteComponent;
-    let fixture: ComponentFixture<ConfirmSubjectDeleteComponent>;
+  let component: ConfirmSubjectDeleteComponent;
+  let fixture: ComponentFixture<ConfirmSubjectDeleteComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [ConfirmSubjectDeleteComponent],
-        }).compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ConfirmSubjectDeleteComponent],
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(ConfirmSubjectDeleteComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmSubjectDeleteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeFalsy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
