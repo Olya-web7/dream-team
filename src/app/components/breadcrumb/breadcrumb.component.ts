e079a6c5c1def19910293a6704346c5a7cabef32
Olya <ola.shy@yahoo.com>
2022-06-10 09:03:32 +0200

    husky

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-breadcrumb',
-  templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.scss']
+    selector: 'app-breadcrumb',
+    templateUrl: './breadcrumb.component.html',
+    styleUrls: ['./breadcrumb.component.scss'],
 })
-export class BreadcrumbComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BreadcrumbComponent {
+    constructor() {}
 }
--- src/app/components/subjects/confirm-subject-delete/confirm-subject-delete.component.ts
+++ src/app/components/subjects/confirm-subject-delete/confirm-subject-delete.component.ts
@@ -1,44 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { SubjectsService } from 'src/app/services/subjects.service';
 
 @Component({
-  selector: 'app-confirm-subject-delete',
-  templateUrl: './confirm-subject-delete.component.html',
-  styleUrls: ['./confirm-subject-delete.component.scss'],
+    selector: 'app-confirm-subject-delete',
+    templateUrl: './confirm-subject-delete.component.html',
+    styleUrls: ['./confirm-subject-delete.component.scss'],
 })
-export class ConfirmSubjectDeleteComponent implements OnInit {
-  subscription = new Subscription();
-  subjectId: string = '';
-  subjectName: string = '';
+export class ConfirmSubjectDeleteComponent implements OnInit, OnDestroy {
+    subscription = new Subscription();
+    subjectId: string = '';
+    subjectName: string = '';
 
-  constructor(
-    private subjectsService: SubjectsService,
-    public dialogRef: MatDialogRef<ConfirmSubjectDeleteComponent>
-  ) {}
+    constructor(
+        private subjectsService: SubjectsService,
+        public dialogRef: MatDialogRef<ConfirmSubjectDeleteComponent>
+    ) {}
 
-  ngOnInit(): void {
-    this.subjectId = this.subjectsService.subjectIdDel;
-    this.subjectName = this.subjectsService.subjectNameDel;
-  }
+    ngOnInit(): void {
+        this.subjectId = this.subjectsService.subjectIdDel;
+        this.subjectName = this.subjectsService.subjectNameDel;
+    }
 
-  deleteOneSubject() {
-    this.subscription.add(
-      this.subjectsService
-        .deleteSubject(this.subjectId)
-        .subscribe((data: any) => {
-          console.log('this: ' + data);
-        })
-    );
-    this.dialogRef.close(this.subjectId);
-  }
+    deleteOneSubject() {
+        this.subscription.add(
+            this.subjectsService.deleteSubject(this.subjectId).subscribe((data: any) => {
+                console.log('this: ' + data);
+            })
+        );
+        this.dialogRef.close(this.subjectId);
+    }
 
-  cancel(){
-    this.dialogRef.close();
-  }
+    cancel() {
+        this.dialogRef.close();
+    }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
