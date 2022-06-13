2b9ce0bab6cfef6f7d51f491860d6902805c1ebe
Olya <ola.shy@yahoo.com>
2022-06-13 13:45:30 +0200

    fix

--- src/app/services/subjects.service.spec.ts
+++ src/app/services/subjects.service.spec.ts
@@ -1,47 +1,44 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { SubjectsService } from './subjects.service';
-import { of } from 'rxjs';
-import { SubjectModel } from '../interfaces/subject.model';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 
-let service: SubjectsService;
-let httpClientSpy: jasmine.SpyObj<HttpClient>;
+import { SubjectsService } from './subjects.service';
+import {environment} from "../../environments/environment";
 
 describe('SubjectsService', () => {
+
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    service = new SubjectsService(httpClientSpy);
     TestBed.configureTestingModule({
-        providers: [SubjectsService],
-        imports: [HttpClientModule]
+      providers: [SubjectsService],
+      imports: [HttpClientTestingModule],
     });
   });
 
-  it('should be created', () => {
-    service = TestBed.inject(SubjectsService);
-    expect(service).toBeTruthy();
-  });
+  describe('getSubjects', () => {
+    let subjectsService: SubjectsService;
+    let httpTestingController: HttpTestingController;
+    let mockSubject: any;
 
+    beforeEach(() => {
+      subjectsService = TestBed.get(SubjectsService);
+      httpTestingController = TestBed.get(HttpTestingController);
+      mockSubject = {
+        subject_id: '1',
+        subject_name: 'Вища математика',
+        subject_description: 'Один із фундаментальних предметів',
+      };
+    });
 
-  it('should return expected specialties', (done: DoneFn) => {
-      const expectedSubjects: SubjectModel[] = [
-          {
-            subject_id: '1',
-            subject_name: 'Вища математика',
-            subject_description: 'Один із фундаментальних предметів',
-          }
-      ];
-
-      httpClientSpy.get.and.returnValue(of(expectedSubjects));
-
-      service.getSubjects().subscribe({
-          next: subjects => {
-              expect(subjects)
-              .withContext('expected specialties')
-              .toEqual(expectedSubjects);
-              done();
-          },
-          error: done.fail
+    it('should GET a list of subjects', () => {
+      subjectsService.getSubjects().subscribe((subjects) => {
+        expect(subjects[0]).toEqual(mockSubject);
       });
+
+      const request = httpTestingController.expectOne(`${environment.apiUrl}/Subject/getRecords`);
+      request.flush([mockSubject]);
+      httpTestingController.verify();
+    });
   });
 });
